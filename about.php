054b403123626e36a5e7821d0bcf221e907bfbfb
Ruslan <37831442+RuslanMak@users.noreply.github.com>
2018-03-27 22:40:42 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php
+    $userName = 'Руслан';
+    $userAge = '25';
+    $userEmail = 'makarovemd@gmail.com';
+    $userCity = 'Белая Церковь';
+    $userAbout = 'Учусь но професию веб-разработчика';
+?>
+
+<style>
+    dl {
+        display: table-row;
+    }
+
+    dt, dd {
+        display: table-cell;
+        padding: 5px 10px;
+    }
+</style>
+<h1>Страница пользователя <?= $userName ?></h1>
+
+<dl>
+    <dt>Имя</dt>
+    <dd><?= $userName ?></dd>
+</dl>
+
+<dl>
+    <dt>Возраст</dt>
+    <dd><?= $userAge ?></dd>
+</dl>
+
+<dl>
+    <dt>Адрес электронной почты</dt>
+    <dd><a href="<?= $userEmail ?>"><?= $userEmail ?></a></dd>
+</dl>
+
+<dl>
+    <dt>Город</dt>
+    <dd><?= $userCity ?></dd>
+</dl>
+
+<dl>
+    <dt>О себе</dt>
+    <dd><?= $userAbout ?></dd>
+</dl>
